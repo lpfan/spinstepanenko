7fe863c509a3bc78790d412ad8f271a2ae1f2882
mshchetinin <misha.shchetinin@gmail.com>
2018-03-15 23:27:34 +0200

    Added Flask-Restful

--- be/application/app.py
+++ be/application/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_restful import Api
+
+from .resources import MainPageResource
+
+
+def register_routes(api):
+    api.add_resource(MainPageResource, '/')
+
 
 def create_app():
     app = Flask(__name__)
+    api = Api(app)
+    register_routes(api)
     return app
 
